b703a38919cf045fb47fdfd0ba017d49e5acb71d
Pharaon3 <Pharaon313@outlook.com>
2023-06-12 04:07:53 -0400

    updated

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -2,3 +2,7 @@ function toggle() {
     if($("#sidebarMenu").hasClass('collapse')) $("#sidebarMenu").removeClass("collapse");
     else $("#sidebarMenu").addClass("collapse");
 }
+function collapse(id){
+    if($("#" + id).hasClass("sidebar-collapsed")) $("#" + id).removeClass("sidebar-collapsed");
+    else $("#" + id).addClass("sidebar-collapsed");
+}
