32a3d85419cf6b65bce85e95458ceef8da8a6433
Pharaon3 <Pharaon313@outlook.com>
2023-06-14 17:22:39 -0400

    javascript

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -10,3 +10,17 @@ function toggleBetslip() {
     if($("#right-content-body").hasClass('mobile-hidden')) $("#right-content-body").removeClass("mobile-hidden");
     else $("#right-content-body").addClass("mobile-hidden");
 }
+function countdown() {
+    var interval = setInterval(function () {
+        let height1 = document.getElementById("middle-content-header").clientHeight;
+        let height2 = document.getElementById("middle-content-body").clientHeight;
+        let height3 = document.getElementById("footer").clientHeight;
+        let heightlogo = document.getElementById("logo").clientHeight;
+        $("#sidemenus").css("height", height1 + height2 - heightlogo + 'px');
+        if (window.innerWidth < 786) $("#sidemenus").css("height", height1 + height2 + height3 - heightlogo - 30 + 'px');
+    }, 100);
+}
+function max(a, b){
+    if (a > b) return a;
+    return b;
+}
