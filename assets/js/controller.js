130a1c7105704f612911f6013395849454070878
Pharaon3 <Pharaon313@outlook.com>
2023-06-14 04:25:34 -0400

    page 1

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,8 +1,12 @@
 function toggle() {
-    if($("#sidebarMenu").hasClass('collapse')) $("#sidebarMenu").removeClass("collapse");
-    else $("#sidebarMenu").addClass("collapse");
+    if($("#sidemenus").hasClass('sidemenus-show')) $("#sidemenus").removeClass("sidemenus-show");
+    else $("#sidemenus").addClass("sidemenus-show");
 }
 function collapse(id){
     if($("#" + id).hasClass("sidebar-collapsed")) $("#" + id).removeClass("sidebar-collapsed");
     else $("#" + id).addClass("sidebar-collapsed");
+}
+function toggleBetslip() {
+    if($("#right-content-body").hasClass('mobile-hidden')) $("#right-content-body").removeClass("mobile-hidden");
+    else $("#right-content-body").addClass("mobile-hidden");
 }
