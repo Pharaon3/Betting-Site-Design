3a14dce7e4e52217d7cb8e1fd5f9ae12476aaaac
Pharaon3 <Pharaon313@outlook.com>
2023-06-16 06:25:09 -0400

    width controlled

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -18,7 +18,7 @@ function toggleBetslip() {
     if($("#right-content-body").hasClass('mobile-hidden')) $("#right-content-body").removeClass("mobile-hidden");
     else $("#right-content-body").addClass("mobile-hidden");
 }
-function countdown() {
+function countdown(pagelink="") {
     var interval = setInterval(function () {
         let container = document.getElementById('middle-content');
         var totalHeight = 0;
@@ -31,7 +31,7 @@ function countdown() {
         let heightlogo = document.getElementById("logo").clientHeight;
         $("#sidemenus").css("height", totalHeight - heightlogo + 'px');
         if (window.innerWidth < 786) $("#sidemenus").css("height", totalHeight  + height3 - heightlogo - 30 + 'px');
-
+    if (pagelink=="detail"){
         let container1 = $("#bet-cards");
         if (container1.scrollLeft() == 0) {
             $("#leftbtn").removeClass("bet-card-active");
@@ -40,7 +40,13 @@ function countdown() {
         // console.log("rightdif", rightdif);
         if (rightdif > 20) {
             $("#rightbtn").removeClass("bet-card-active");
-        } else if (!$("#rightbtn").hasClass("bet-card-active")) $("#rightbtn").addClass("bet-card-active");
+        } else if (!$("#rightbtn").hasClass("bet-card-active")) $("#rightbtn").addClass("bet-card-active")
+    }
+    if (window.innerWidth > 1300){
+        $("#middle-content").css("max-width", window.innerWidth - 672);
+    } else if (window.innerWidth > 786){
+        $("#middle-content").css("max-width", window.innerWidth - 287);
+    } else $("#middle-content").css("max-width", "100%");
     }, 100);
 }
 function max(a, b){
