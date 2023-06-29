c079bd4ea3f357049c21fa78d66cd3a5fafbf66c
Pharaon3 <Pharaon313@outlook.com>
2023-06-28 22:06:09 -0400

    bet-card4 in controller removed

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -39,22 +39,27 @@ function countdown(pagelink="") {
         $("#sidemenus").css("height", totalHeight - heightlogo + 'px');
         if (window.innerWidth < 786) $("#sidemenus").css("height", totalHeight  + height3 - heightlogo - 30 + 55 + 'px');
     if (pagelink=="detail"){
-        let container1 = $("#bet-cards");
-        if (container1.scrollLeft() == 0) {
-            $("#leftbtn").removeClass("bet-card-active");
-        } else if (!$("#leftbtn").hasClass("bet-card-active")) $("#leftbtn").addClass("bet-card-active");
-        let rightdif = document.getElementById("bet-cards-button").getBoundingClientRect().left - document.getElementById("bet-card4").getBoundingClientRect().right;
-        // console.log("rightdif", rightdif);
-        if (rightdif > 20) {
-            $("#rightbtn").removeClass("bet-card-active");
-        } else if (!$("#rightbtn").hasClass("bet-card-active")) $("#rightbtn").addClass("bet-card-active")
-    }
-    
+            let container1 = $("#bet-cards");
+            if (container1.scrollLeft() == 0) {
+                $("#leftbtn").removeClass("bet-card-active");
+            } else if (!$("#leftbtn").hasClass("bet-card-active")) $("#leftbtn").addClass("bet-card-active");
+            let cards = document.getElementById("bet-cards").children;
+            lastcardwidth = 0;
+            if (cards.length) {
+                let lastcard = cards[cards.length - 1];
+                lastcardwidth = lastcard.getBoundingClientRect().right;
+            }
+            let rightdif = document.getElementById("bet-cards-button").getBoundingClientRect().left - lastcardwidth;
+            if (rightdif > 20) {
+                $("#rightbtn").removeClass("bet-card-active");
+            } else if (!$("#rightbtn").hasClass("bet-card-active")) $("#rightbtn").addClass("bet-card-active")
+        }
+
     if (window.innerWidth > 1300){
-        $("#middle-content").css("max-width", window.innerWidth - 672);
+            $("#middle-content").css("max-width", window.innerWidth - 672);
     } else if (window.innerWidth > 786){
-        $("#middle-content").css("max-width", window.innerWidth - 287);
-    } else $("#middle-content").css("max-width", "100%");
+            $("#middle-content").css("max-width", window.innerWidth - 287);
+        } else $("#middle-content").css("max-width", "100%");
 
     }, 1);
 }
